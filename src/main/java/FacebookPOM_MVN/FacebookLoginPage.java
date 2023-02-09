56ebe9d7e884587b601bbf7fb82bd9e7c9c8fa61
Mahesh <maheshshelke8600@gmail.com>
2023-02-09 22:22:17 +0530

    Log is Added

--- src/main/java/FacebookPOM_MVN/FacebookLoginPage.java
+++ src/main/java/FacebookPOM_MVN/FacebookLoginPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
 
 
 public class FacebookLoginPage {
@@ -22,7 +23,7 @@ public class FacebookLoginPage {
 	public void EnterID(String ID) throws EncryptedDocumentException, IOException {
 		
 		IDTab.sendKeys(ID);
-		
+		Reporter.log("Mobile number is entered", true);
 	}
 public void EnterPass(String Pass) throws EncryptedDocumentException, IOException {
 		
